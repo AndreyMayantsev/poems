19bf90ae0dd7f24947496844792849e1b91412f9
Andrey Mayantsev <dumspiro2019@gmail.com>
2023-06-01 21:07:34 +0300

    Added module for checking devices type, added check in initialize

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import MainWindow from './components/MainWindow.vue';
 import { requestType } from './Core-prod/api/dataTypes';
 import { HttpRequestFactory } from './Core-prod/api/requests/HttpRequestFactory';
 import { ConsoleLogger } from './Core-prod/Logger/ConsoleLogger';
+import { CheckDeviceType } from './Core-prod/CheckDeviceType'
 
 
 export default {
@@ -40,22 +41,30 @@ export default {
     ConsoleLogger.writeLogInfo("Создан пользователь, авторизуйтесь!\n " + JSON.stringify(UserInstance.getPublicInfo()));
     ConsoleLogger.writeLogInfo("=== Загрузка данных ===");
     
-    //  useless?
-    if(localStorage.getItem('userID') !== null) {  
-        this.$store.commit( 'SET_USER_ID', localStorage.getItem('userID') );
-        ConsoleLogger.writeLogInfo("Обнаружен ID в LocalStorage: " + this.$store.getters.USER_ID);
-    }
+    // whatTheDevice - Computer PC or Mobile Device
+    if(CheckDeviceType.isMobileDevice()) { 
+        localStorage.setItem('mobileDevice', true);
+    } else {
+        localStorage.setItem('mobileDevice', false);
+    } 
+
+    // //  useless?
+    // if(localStorage.getItem('userID') !== null) {  
+    //     this.$store.commit( 'SET_USER_ID', localStorage.getItem('userID') );
+    //     ConsoleLogger.writeLogInfo("Обнаружен ID в LocalStorage: " + this.$store.getters.USER_ID);
+    // }
 
     let testAuth = await HttpRequestFactory.makeRequest(requestType.RoomsGet, { limit:1, offset:0 })
-
     
     if(testAuth.success) {
-        ConsoleLogger.writeLogInfo("Проверка авторизации прошла успешно!");
+        //localStorage.setItem('userID', testAuth.user_id); // this is no auth request and NO CONTENTS user_id
+        ConsoleLogger.writeLogInfo("Проверка авторизации прошла успешно! ");
         this.$store.commit( 'SET_USER_ID', localStorage.getItem('userID') );
     } else {
         ConsoleLogger.writeLogWarning("Проверка авторизации ПРОВАЛЕНА! Код: " + testAuth.code);
         localStorage.removeItem('userID');
         localStorage.removeItem('auth');
+        //$router.push({ name:'auth' });
     }
     
     // Попробовать встроить сюда тест активности сессии
